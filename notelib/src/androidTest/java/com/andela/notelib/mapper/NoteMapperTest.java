69ec47240cc9ea981470c4d31eb6acdf50fcdac2
andela <kehinde.ogunde@andela.com>
2016-02-13 15:56:25 +0100

    Add Mapper class test

---
+++ notelib/src/androidTest/java/com/andela/notelib/mapper/NoteMapperTest.java
@@ -0,0 +1,29 @@
+package com.andela.notelib.mapper;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by andela on 2/13/16.
+ */
+public class NoteMapperTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testReadNote() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testWriteNote() throws Exception {
+
+    }
+}
