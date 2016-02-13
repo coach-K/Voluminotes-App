25e47bf5c78cc4e49e1651d90e3703edbb28ae6c
andela <kehinde.ogunde@andela.com>
2016-02-13 15:55:10 +0100

    Add Note class

---
+++ notelib/src/main/java/com/andela/notelib/note/Note.java
@@ -0,0 +1,47 @@
+package com.andela.notelib.note;
+
+
+import android.os.Parcelable;
+
+public abstract class Note implements Parcelable {
+    private int id;
+    private int folderId;
+    private int trash;
+    private long date;
+
+    public Note() {
+        this.date = System.currentTimeMillis();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public int getFolderId() {
+        return folderId;
+    }
+
+    public void setFolderId(int folderId) {
+        this.folderId = folderId;
+    }
+
+    public int getTrash() {
+        return trash;
+    }
+
+    public void setTrash(int trash) {
+        this.trash = trash;
+    }
+}
