47859bec061aab281422c7e206d27ceddd97e654
andela <kehinde.ogunde@andela.com>
2016-02-13 16:39:21 +0100

    Write Test to fail NoteMapper class

--- notelib/src/androidTest/java/com/andela/notelib/mapper/NoteMapperTest.java
+++ notelib/src/androidTest/java/com/andela/notelib/mapper/NoteMapperTest.java
@@ -1,29 +1,41 @@
 package com.andela.notelib.mapper;
 
-import static org.junit.Assert.*;
+import android.test.ApplicationTestCase;
+
+import com.andela.notelib.note.PaperNote;
+
+import junit.framework.TestCase;
 
 /**
  * Created by andela on 2/13/16.
  */
-public class NoteMapperTest {
+public class NoteMapperTest extends TestCase {
 
-    @org.junit.Before
-    public void setUp() throws Exception {
+    PaperNote paperNote;
+    NoteMapper<PaperNote> noteMapper;
 
+    @Override
+    protected void setUp() throws Exception {
+        paperNote = new PaperNote("Everybody","Beautiful people", "Arial", 23, 0);
+        noteMapper = new NoteMapper<>();
+        super.setUp();
     }
 
-    @org.junit.After
-    public void tearDown() throws Exception {
 
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
     }
 
-    @org.junit.Test
     public void testReadNote() throws Exception {
-
+        String strPaper = noteMapper.readNote(paperNote);
+        assertNotNull(strPaper);
     }
 
-    @org.junit.Test
     public void testWriteNote() throws Exception {
-
+        String strPaper = noteMapper.readNote(paperNote);
+        PaperNote newPaperNote = noteMapper.writeNote(strPaper, PaperNote.class);
+        assertEquals(newPaperNote.getNote(), paperNote.getNote());
     }
+
 }
