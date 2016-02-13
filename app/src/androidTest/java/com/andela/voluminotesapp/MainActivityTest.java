8439748571a5a0523fa3e248d3922828815426d7
andela <kehinde.ogunde@andela.com>
2016-02-13 14:39:36 +0100

    Creates new android project [Starts #113639971]

---
+++ app/src/androidTest/java/com/andela/voluminotesapp/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.andela.voluminotesapp;
+
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import junit.framework.TestCase;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by andela on 2/12/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest extends TestCase {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldBeAbleToLaunchMainActivity() {
+        onView(withText("Hello")).check(ViewAssertions.matches(isDisplayed()));
+    }
+}
