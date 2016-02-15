d4a3ae7f0cd875fe14fd92e95b301f4c35b5889e
andela <kehinde.ogunde@andela.com>
2016-02-15 05:02:33 +0100

    Add MessageBox Class and ApplicationExit Service

---
+++ app/src/main/java/com/andela/voluminotesapp/utilities/ApplicationExit.java
@@ -0,0 +1,33 @@
+package com.andela.voluminotesapp.utilities;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.andela.voluminotesapp.activities.MyApplication;
+
+/**
+ * Created by andela on 2/14/16.
+ */
+public class ApplicationExit extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void onTaskRemoved(Intent rootIntent) {
+        MyApplication.getNoteManager(getBaseContext()).saveChanges();
+        stopSelf();
+    }
+}
---
+++ app/src/main/java/com/andela/voluminotesapp/utilities/MsgBox.java
@@ -0,0 +1,15 @@
+package com.andela.voluminotesapp.utilities;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MsgBox {
+    private static Toast toast;
+    public static void show(Context context, String message){
+        if (toast != null)
+            toast.cancel();
+        toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
