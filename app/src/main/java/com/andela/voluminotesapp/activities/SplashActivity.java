f4445b79980c912216f9e0a171679680084565ab
andela <kehinde.ogunde@andela.com>
2016-02-15 05:37:29 +0100

    Refactor Splash Activity

--- app/src/main/java/com/andela/voluminotesapp/activities/SplashActivity.java
+++ app/src/main/java/com/andela/voluminotesapp/activities/SplashActivity.java
@@ -3,10 +3,15 @@ package com.andela.voluminotesapp.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.andela.voluminotesapp.utilities.Launcher;
+
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Launcher.launch(SplashActivity.this, MainActivity.class);
+        finish();
     }
 }
