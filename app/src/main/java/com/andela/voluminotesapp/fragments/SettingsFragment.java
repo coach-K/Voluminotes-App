14473d3b849aebb3d0dc0dba25098ea490c7b2b1
andela <kehinde.ogunde@andela.com>
2016-02-15 05:36:27 +0100

    Complete Auto Save Implementation

---
+++ app/src/main/java/com/andela/voluminotesapp/fragments/SettingsFragment.java
@@ -0,0 +1,26 @@
+package com.andela.voluminotesapp.fragments;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andela.voluminotesapp.R;
+
+
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_fragment, container, false);
+    }
+}
