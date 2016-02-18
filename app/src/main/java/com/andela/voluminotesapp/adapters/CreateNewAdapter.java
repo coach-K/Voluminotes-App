a8edcc43452e2a632eb109ec7edaa773b96ef283
andela <kehinde.ogunde@andela.com>
2016-02-18 11:04:33 +0100

    Refactor CreateNewAdapter class

--- app/src/main/java/com/andela/voluminotesapp/adapters/CreateNewAdapter.java
+++ app/src/main/java/com/andela/voluminotesapp/adapters/CreateNewAdapter.java
@@ -4,13 +4,9 @@ package com.andela.voluminotesapp.adapters;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
 
 import com.andela.notelib.note.Folders;
-import com.andela.voluminotesapp.fragments.AudioNoteFragment;
-import com.andela.voluminotesapp.fragments.DrawNoteFragment;
 import com.andela.voluminotesapp.fragments.PaperNoteFragment;
-import com.andela.voluminotesapp.fragments.TodoNoteFragment;
 
 
 public class CreateNewAdapter extends FragmentPagerAdapter {
@@ -26,15 +22,6 @@ public class CreateNewAdapter extends FragmentPagerAdapter {
         switch (this.type) {
             case Folders.STATIC_PAPER_NOTE:
                 fragment = new PaperNoteFragment();
-                break;
-            case Folders.STATIC_TODO_NOTE:
-                fragment = new TodoNoteFragment();
-                break;
-            case Folders.STATIC_AUDIO_NOTE:
-                fragment = new AudioNoteFragment();
-                break;
-            case Folders.STATIC_DRAW_NOTE:
-                fragment = new DrawNoteFragment();
                 break;
             default:
                 break;
